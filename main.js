dedadfa51cecd439ad13bb8ead1d1cff48caf742
TranTamNhu <tamnhu2722005@gmail.com>
2025-01-26 21:07:10 +0700

    fix

--- main.js
+++ main.js
@@ -21,3 +21,46 @@ cards.forEach((card)=>{
         
     })
 })
+
+var form = document.getElementById("trial-form");
+var confirmationMessage = document.getElementById("confirmation-message");
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+   
+    const company = document.getElementById("company").value;
+    const email = document.getElementById("email").value;
+    const launch = document.getElementById("launch").value;
+
+    if (company && email && launch) {
+       
+        confirmationMessage.style.display = "block";
+
+       
+        form.style.display = "none";
+    }
+});
+const contactForm = document.getElementById("contact-us");
+const confirmation = document.getElementById("contact-success");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+  
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const launch = document.getElementById("launch").value;
+    const message = document.getElementById("message").value;
+
+    
+    if (name && phone && email && launch && message) {
+        
+        confirmation.style.display = "block";
+
+        
+        contactForm.style.display = "none";
+    } 
+});
